0c5a9b7e89e13a728f0a73900025e82d68ec120b
AgustinSilva97 <enriqueagustinsilva@gmail.com>
2016-07-09 11:30:09 -0300

    commit Ventanas OT
    Agregue las ventanas: "Nuevo_Profesor" y "Nuevo_Alumno"

---
+++ codigo fuente/Titan/Titan/Oficina Tecnica/Nuevo_Proveedor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Titan.Oficina_Tecnica
+{
+    public partial class Nuevo_Proveedor : Form
+    {
+        public Nuevo_Proveedor()
+        {
+            InitializeComponent();
+        }
+
+        private void Nuevo_Proveedor_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
